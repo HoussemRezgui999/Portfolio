e085aaddeffacd2e0339ef5ff6b26bf70a8532ea
Houssem Rezgui <houssemrezgui2015@gmail.com>
2025-04-20 19:21:29 +0100

    more changes

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Keep it '/' if deploying to root
   plugins: [react()],
-})
+});
+
